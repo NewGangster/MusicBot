30744d86ac41970d4dc204f4ef6239046abda864
GANGSTER <97881362+NewGangster@users.noreply.github.com>
2022-01-20 22:07:43 +0530

    Update jebot.py

--- jebot.py
+++ jebot.py
@@ -254,7 +254,7 @@ async def help(client, message):
                chat_id=message.chat.id,
                text="""<b>Send a song name to download song
 
-@akittybot</b>""",
+@GangsterTelegram_bot</b>""",
             reply_to_message_id=message.message_id
         )
     else:
